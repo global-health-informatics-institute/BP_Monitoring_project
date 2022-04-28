d65860679d3cf259ace74ad9b73c3a33b9ff7959
veechinoko <vituchinoko94@gmail.com>
2022-04-28 10:19:44 +0200

    Resolved Scanning window

--- BP/main.py
+++ BP/main.py
@@ -19,69 +19,39 @@ class MainWindow(Screen):
 
 
 class ScanWindow(Screen):
-
     def callback(self):
-        print('Enter your something:')
-        name = input()
+        name = self.manager.get_screen("Scan").ids["textFocus"].text
         val = name.split('~')
         print(len(val))
         print(val)
-        firstname = val[6]
-        funame = firstname.replace(',', ' ')
-        lastname = val[4]
-        fname = funame + " " + lastname
-        N_id = val[5]
-        gender = val[8]
-        DOB = val[9]
-        BP = 0
-
         if len(val) == 12:
+            firstname = val[6]
+            funame = firstname.replace(',', ' ')
+            lastname = val[4]
+            fname = funame + " " + lastname
+            N_id = val[5]
+            gender = val[8]
+            DOB = val[9]
+            BP = 0
             cur.execute("SELECT * FROM Patients WHERE id=%s", [N_id])
             record = cur.fetchall()
             if record:
-                self.manager.ids.scn.N_id.text = N_id
-                self.manager.ids.scn.f_name.text = fname
-                self.manager.ids.scn.gender.text = gender
-                self.manager.ids.scn.DOB.text = DOB
-                self.manager.ids.scn.BP.text = BP
                 self.parent.current = "Patient_Details"
 
             else:
                 cur.execute("INSERT INTO Patients (id, Full_name, Gender, DOB, BP) VALUES (%s, %s, %s, %s, %s) ",
                             (N_id, fname, gender, DOB, BP))
                 db.commit()
-                self.manager.ids.scn.N_id.text = N_id
-                self.manager.ids.scn.f_name.text = fname
-                self.manager.ids.scn.gender.text = gender
-                self.manager.ids.scn.dob.text = DOB
-                self.manager.ids.scn.bp.text = BP
+                db.close()
                 self.parent.current = "Patient_Details"
 
         else:
+            self.manager.get_screen("Scan").ids["textFocus"].text = ""
+            self.manager.get_screen("Scan").ids["textFocus"].text
             self.parent.current = "Scan"
-            self.callback()
 
 
 class PatientDetails(Screen):
-    # def callback(self):
-
-    # N_id = self.manager.
-    # fname = ""
-    # gender = ""
-    # DOB = ""
-    # BP = ""
-    #
-    # cur.execute("SELECT * FROM Patients WHERE id=%s", [N_id])
-    # record = cur.fetchall()
-    # if record:
-    #     self.natID.text = N_id
-    #     self.fName.text = fname
-    #     self.gen.text = gender
-    #     self.dOB.text = DOB
-    #     self.bloodP.text = BP
-    #
-    # else:
-    #     self.parent.current = "Scan_window"
     pass
 
 
