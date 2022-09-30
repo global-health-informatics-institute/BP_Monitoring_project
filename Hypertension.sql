a35e12f1c8c6217b5c7c45a0e22009075e8e71e9
Thembi01 <111663089+Thembi01@users.noreply.github.com>
2022-09-30 12:17:14 +0200

    Changed gender data type in Demographic table

--- Hypertension.sql
+++ Hypertension.sql
@@ -58,7 +58,7 @@ CREATE TABLE `vitals` (
   PRIMARY KEY (`v_id`),
   KEY `Foreign_Key` (`id`),
   CONSTRAINT `Foreign_Key` FOREIGN KEY (`id`) REFERENCES `Demographic` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
