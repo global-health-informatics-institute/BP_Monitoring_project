95f06903a0a05e12a9518de37fe606c453be03fa
Thembi01 <sithembiphombeya@gmail.com>
2024-04-17 16:20:25 +0200

    fixed server sms processing

--- BP_server/BP_server_sms.py
+++ BP_server/BP_server_sms.py
@@ -5,7 +5,6 @@ from configparser import ConfigParser
 import json
 
 
-
 def initialize_settings():
     settings = {}
     with open("/home/pi/BP_Monitoring_project/BP_server/conn.config") as json_file:
@@ -30,13 +29,39 @@ config = ConfigParser()
 def parseSms(ele):
     splitdata = ele.split("|")
     print(splitdata)
-    cur.execute("INSERT INTO smsload (n_id, vitals, bp_cat, fullname, gender, dob)"
-                "VALUES (%s, %s, %s, %s, %s, %s)",
-                (splitdata[0], splitdata[1], splitdata[2], splitdata[3], splitdata[4], splitdata[5]))
+    cur.execute("INSERT INTO smsload (n_id, vitals, bp_cat, fullname, gender, dob, nat_id, p_rate)"
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                (splitdata[0], splitdata[1], splitdata[2], splitdata[3], splitdata[4], splitdata[5], splitdata[6], splitdata[7]))
     db.commit()
     print("sent to database")
 
+def acknowledgement(patient_name):
+    responses = []
+    outgoingmsg = "vitals received for " + patient_name
+    responses.append(outgoingmsg)
+    print("this is responses: ", responses)
+    
+    cmd = 'AT+CMGF=1\r'
+    ser_port.write(cmd.encode())
+    msg = ser_port.read(64)
+    time.sleep(0.1)
+
+    cmd1 = 'AT+CMGS="'+settings["client_number"]+'"\r'
+    ser_port.write(cmd.encode())
+    msg = ser_port.read(64)
+    time.sleep(0.1)
+
+    
+    for i in responses:
+        ser_port.write(str.encode(i))
+        msgout = ser_port.read(1000)
+        time.sleep(0.1)
+        print("this is msgout in i loop: ", msgout)
+        print("response sent")
+        time.sleep(5)
+
 def textmode():
+    print("staring textmode")
     cmd = 'AT+CMGF=1\r'
     ser_port.write(cmd.encode())
     msg = ser_port.read(64)
@@ -46,46 +71,31 @@ def textmode():
     ser_port.write(str.encode(readSMS))
     time.sleep(5)
     msgread = ser_port.read(64)
+    
     time.sleep(5)
 
     a = ser_port.readlines()
+        
     for element in a:
+        print("Starting loop: ", a)
         seperator = '|'
         sep = seperator.encode()
         if sep in element:
             ele = element.decode()
+            print(ele)
             parseSms(ele)
-            splitdata = ele.split("|")
-            
-            cmd = 'AT+CMGF=1\r'
-            ser_port.write(cmd.encode())
-            msg = ser_port.read(64)
-            time.sleep(0.1)
-    
-            # cmd = 'AT+CMGS=\r'
-            cmd1 = 'AT+CMGS="'+settings["client_number"]+'"\r'
-            ser_port.write(cmd.encode())
-            msg = ser_port.read(64)
-            time.sleep(0.1)
-    
-            outgoingmsg = "vitals received for " + splitdata[3]
-            ser_port.write(str.encode(outgoingmsg))
-            msgout = ser_port.read(1000)
-            time.sleep(0.1)
-            print(msgout)
-    
-            ser_port.write(str.encode("\x1A"))
-            read_port = ser_port.read(5)
-    
-            print("response sent")
-            time.sleep(5)
-            
+            splitdata = ele.split(seperator)
+
             delcom = 'AT+CMGD=1,4\r'
             ser_port.write(delcom.encode())
             delmsg = ser_port.read(64)
-            time.sleep(0.1)
+            print(delmsg)
+            time.sleep(5)
             print("All deleted")
+
+            acknowledgement(splitdata[3])
+    
             
 while True:
     textmode()
-            
+    
